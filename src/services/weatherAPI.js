8a338b408b94feee424f17afd5f4d47332dd375b
Omar Mostafa <103866558+RWELYY@users.noreply.github.com>
2025-12-17 02:12:57 +0400

    Update weatherAPI.js

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "https://api.openweathermap.org/data/2.5/weather?lat=24.7&lon=46.5&appid={API_KEY}";
+  "https://api.openweathermap.org/data/2.5/weather?lat=24.7&lon=46.5&appid=b8efc4933a3f228f4c388b6f06771639";
 
 export function getWeather(cancelToken) {
   return axios
